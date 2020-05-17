bc0f9c53264ca0b0c1569ed7dff47ebbc4a7a624
kim-misol <wonderfulpine@gmail.com>
2020-05-17 22:23:17 +0900

    Specified the type for sumNumbersInArray.dart

--- test/sumNumbersInArray.dart
+++ test/sumNumbersInArray.dart
@@ -2,7 +2,7 @@ main() {
   print(sumNumbersInArray([3, 4, 5, 6]));
 }
 
-int sumNumbersInArray(var arr) {
+int sumNumbersInArray(List<int> arr) {
   var result = 0;
 
   for (int i = 0; i < arr.length; i++) {
